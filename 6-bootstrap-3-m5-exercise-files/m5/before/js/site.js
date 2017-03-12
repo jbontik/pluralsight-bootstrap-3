c2a6fb53ffad56ef65c6f549cfba4019950c3b7f
jbont <jbont@jbont>
2017-03-12 18:29:37 +0100

    tooltip:   required to use javascript, with tooltip() function   + data-toggle = tooltip

--- 6-bootstrap-3-m5-exercise-files/m5/before/js/site.js
+++ 6-bootstrap-3-m5-exercise-files/m5/before/js/site.js
@@ -9,7 +9,8 @@
     });
 
     var $sentDialog = $("#sentDialog");
-    $("#contactForm").on("submit", function () {
+    var contactForm = $("#contactForm");
+    contactForm.on("submit", function () {
         $sentDialog.modal("show");
         return false;
     });
@@ -17,5 +18,11 @@
     // $sentDialog.on("hidden.bs.modal", function () {
     //     alert("close");
     // })
+
+    contactForm.find("input[type=submit]").tooltip(
+        {
+            delay: {show: 500, hide: 0}
+        }
+    );
 
 })();
