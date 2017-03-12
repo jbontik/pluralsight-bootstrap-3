ee88289057c6f3176ffb5989b3edeb83ebf6230f
jbont <jbont@jbont>
2017-03-12 09:16:35 +0100

    Use javascript to show dialog on submit event

--- 6-bootstrap-3-m5-exercise-files/m5/before/js/site.js
+++ 6-bootstrap-3-m5-exercise-files/m5/before/js/site.js
@@ -1,11 +1,21 @@
 (function () {
-  "use strict";
-  
-  var $pickButton = $("#pickButton");
+    "use strict";
 
-  $("#reasonDropdown li a").on("click", function () {
-    var reason = $(this).text();
-    $pickButton.text(reason);
-  });
+    var $pickButton = $("#pickButton");
+
+    $("#reasonDropdown li a").on("click", function () {
+        var reason = $(this).text();
+        $pickButton.text(reason);
+    });
+
+    var $sentDialog = $("#sentDialog");
+    $("#contactForm").on("submit", function () {
+        $sentDialog.modal("show");
+        return false;
+    });
+
+    // $sentDialog.on("hidden.bs.modal", function () {
+    //     alert("close");
+    // })
 
 })();
